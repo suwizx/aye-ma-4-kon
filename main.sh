ae3d809f77c13de544bc21334feaaf08d0564eb4
Suwijak Promsatid <suwijak.pak@gmail.com>
2025-11-20 17:16:12 +0700

    fix sscript

--- main.sh
+++ main.sh
@@ -6,8 +6,10 @@ DISK_ALL=$(df -h / | tail -n 1 | awk '{print $2}')
 DISK_USEAGE=$(df -h / | tail -n 1 | awk '{print $3}')
 CPU_USAGE=$(mpstat 1 1 | awk '/Average/ {print 100-$12"%"}')
 MEMORY_USAGE=$(free -h)
+UPTIME=$(uptime | awk '{print $3}' | sed 's/,$//')
 
 echo $DISK_ALL
 echo $DISK_USAGE
 echo $CPU_USAGE
 echo $MEMORY_USAGE
+echo $UPTIME
