728c21627c7efd044a1ca57cca9190d759b0f501
Phoz07 <maverxk07@gmail.com>
2025-11-20 16:53:46 +0700

    Fux Big

--- main.sh
+++ main.sh
@@ -2,5 +2,8 @@ echo "push status start ..."
 
 CPU_ALL=$(df -h / | tail -n 1 | awk '{print $2}')
 CPU_USAGE=$(df -h / | tail -n 1 | awk '{print $3}')
+MEMORY_USAGE=$(free -h)
+
 echo $CPU_ALL
 echo $CPU_USAGE
+echo $MEMORY_USAGE
