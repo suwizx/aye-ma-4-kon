43a22f161afa08c003437a43c68af94fc37fe526
Suwijak Promsatid <suwijak.pak@gmail.com>
2025-11-20 18:25:49 +0700

    ip

--- main.sh
+++ main.sh
@@ -6,8 +6,12 @@ DISK_ALL=$(df -h / | tail -n 1 | awk '{print $2}')
 DISK_USEAGE=$(df -h / | tail -n 1 | awk '{print $3}')
 CPU_USAGE=$(mpstat 1 1 | awk '/Average/ {print 100-$12"%"}')
 MEMORY_USAGE=$(free -h)
+EXT_IP=$(echo | curl -s https://ipinfo.io/ip)
+INT_IP=$(hostname -I)
 
 echo $DISK_ALL
 echo $DISK_USAGE
 echo $CPU_USAGE
 echo $MEMORY_USAGE
+echo $EXT_IP
+echo $INT_IP
